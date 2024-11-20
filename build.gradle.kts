374b8578ddf81c676e45047263b0a67b2522bc50
Alberto Esquivias <alberto.m.esquivias@gmail.com>
2024-11-19 19:37:39 -0800

    updates gradle (missed one)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinSerialization) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.org.jetbrains.kotlin.kapt) apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) apply false
 }
